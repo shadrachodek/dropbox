3cdbcf5330987bc6eefcf674e70c6a4dae9a3333
Shadrach Odekhiran <shadrach.odekhiran@gmail.com>
2021-01-13 23:52:03 +0100

    clearing finished upload and closing the modal

--- resources/views/livewire/file-browser.blade.php
+++ resources/views/livewire/file-browser.blade.php
@@ -166,7 +166,16 @@
             wire:ignore
             x-data="{
                 initFilepond () {
+
                     const pond = FilePond.create(this.$refs.filepond, {
+                        onprocessfile: (error, file) => {
+                            pond.removeFile(file.id)
+
+                            if (pond.getFiles().length === 0) {
+                                @this.set('showingFileUploadForm', false)
+                            }
+                        },
+                        allowRevert: false,
                         server: {
                             process: (fieldName, file, metdate, load, error, progress, abort, transfer, options) => {
                                 @this.upload('upload', file, load, error, progress)
